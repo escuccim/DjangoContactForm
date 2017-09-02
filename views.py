5b58080944568127f6eddd1ef6c17466e5d344c7
Eric Scuccimarra <skooch@gmail.com>
2017-09-02 17:27:55 +0200

    Added validation

--- views.py
+++ views.py
@@ -10,30 +10,36 @@ from django.core.mail import EmailMessage
 from django.template import Context
 from django.template.loader import get_template
 from django.conf import settings
+from django.template import loader
 
 def Contact(request):
+    errors = None
+    contact_name = ''
+    contact_email = ''
+    contact_subject = ''
+    contact_content = ''
+
     if request.method == 'POST':
         contact_form = ContactForm(data=request.POST)
+        contact_name = request.POST.get('name', '')
+        contact_email = request.POST.get('email', '')
+        contact_content = request.POST.get('message', '')
+        contact_subject = request.POST.get('subject', '')
 
         if contact_form.is_valid():
-            contact_name = request.POST.get('name', '')
-            contact_email = request.POST.get('email', '')
-            contact_content = request.POST.get('message', '')
-            contact_subject = request.POST.get('subject', '')
+            message_details = {'contact_name': contact_name,'contact_email': contact_email,'form_content': contact_content,}
+            html_message = loader.render_to_string('contact/contact_template.html', message_details)
 
-            template = get_template('contact/contact_template.html')
-            context = {'contact_name': contact_name,'contact_email': contact_email,'form_content': contact_content,}
-            content = template.render(context)
-
-            email = EmailMessage("New contact form submission",content,"Contact message <" + contact_email + ">",[settings.DEFAULT_FROM_EMAIL],headers={'Reply-To': contact_email})
-            email.send()
+            send_mail(contact_subject, contact_content, contact_email, [settings.DEFAULT_FROM_EMAIL], fail_silently=True, html_message=html_message)
 
             contact_form = ContactForm()
             return render(request, 'contact/contact.html', { 'contact_form' : contact_form, 'message' : 'Your message has been sent'})
         else:
-            print contact_form.errors
+            errors = contact_form.errors
 
     else:
         contact_form = ContactForm()
 
-    return render(request, 'contact/contact.html', { 'contact_form' : contact_form, 'message' : None})
+    context_dict = { 'contact_form' : contact_form, 'message' : None, 'errors' : errors, 'contact_name' : contact_name, 'contact_email': contact_email, 'contact_subject': contact_subject, 'contact_content': contact_content}
+
+    return render(request, 'contact/contact.html', context_dict )
